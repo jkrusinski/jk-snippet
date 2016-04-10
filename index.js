debc0061217cd3c1dc233f925e9a05693a0bb0a0
Jerry Krusinski <jkruzr@gmail.com>
2016-04-10 12:22:36 -0500

    Completed tutorial

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
-#!/usr/bin/env node
-console.log('Hello World!');
+#!/usr/bin/env node --harmony
+var fs = require('fs');
+var ProgressBar = require('progress');
+var co = require('co');
+var prompt = require('co-prompt');
+var program = require('commander');
+var request = require('superagent');
+var chalk = require('chalk');
+
+program
+    .arguments('<file>')
+    .option('-u, --username <username>', 'The user to authenticate as')
+    .option('-p, --password <password>', 'The user\'s password')
+    .action(function(file) {
+        co(function* () {
+            var username = yield prompt('username: ');
+            var password = yield prompt.password('password: ');
+
+            var fileSize = fs.statSync(file).size;
+            var fileStream = fs.createReadStream(file);
+            var barOpts = {
+                width: 20,
+                total: fileSize,
+                clear: true
+            };
+
+            var bar = new ProgressBar(' uploading [:bar] :percent :etas', barOpts);
+
+            fileStream.on('data', function (chunk) {
+                bar.tick(chunk.length);
+            });
+
+            request
+                .post('https://api.bitbucket.org/2.0/snippets/')
+                .auth(username, password)
+                .attach('file', fileStream)
+                .set('Accept', 'application/json')
+                .end(function(err, res) {
+                    if (!err && res.ok) {
+                        var link = res.body.links.html.href;
+                        console.log(chalk.bold.cyan('Snippet created: ') + link);
+                        process.exit(0);
+                    }
+
+                    var errorMessage;
+                    if (res && res.status === 401) {
+                        errorMessage = "Authentication failed! Bad username or password.";
+                    } else if (err) {
+                        errorMessage = err;
+                    } else {
+                        errorMessage = res.text;
+                    }
+                    console.log(chalk.red(errorMessage));
+                    process.exit(0);
+                });
+        });
+    })
+    .parse(process.argv);
